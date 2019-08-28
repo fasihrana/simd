840d806e26a3fb95ec33921a35d7954b0f73af10
Fasih Rana <fasih@apparicion.com>
2019-08-28 14:40:28 +0100

    changed to add the align method for array

--- benches/example.rs
+++ benches/example.rs
@@ -4,17 +4,24 @@ use bencher::*;
 const len: usize = 10000000;
 
 fn normal_add_bench(b: &mut Bencher) {
-    b.iter(||{
-        let mut arr = vec![1;len];
+    let mut arr = vec![1;len];
+    b.iter(move ||{
+
         normal_add(&mut arr, 1); 
     });    
 }
 
 fn simd_add_bench(b:&mut Bencher) {
-    b.iter(||{
-        let mut arr = vec![1;len];
-        unsafe{simd_add(&mut arr, 1)};
-    });    
+    unsafe {
+        let mut arr = vec![1; len];
+        let (mut prefix, mut arr, mut suffix) = arr.align_to_mut::<i32>();
+
+        b.iter(move || {
+            normal_add(prefix, 1);
+            simd_add(&mut arr, 1);
+            normal_add(suffix, 1);
+        });
+    }
 }
 
 benchmark_group!(benches,normal_add_bench, simd_add_bench);
--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,12 @@ pub unsafe fn simd_add(arr:&mut [i32], val: i32) {
     #[cfg(target_arch = "x86_64")]
     use core::arch::x86_64::*;
 
+    //let (mut prefix, mut arr, mut suffix) = arr.align_to_mut::<i32>();
+    //normal_add(prefix, val);
+
     let a = _mm256_set_epi32(val,val,val,val,val,val,val,val);
     for chunk in arr.chunks_exact_mut(8) {
+
         let mut b =_mm256_set_epi32(chunk[7], chunk[6], chunk[5], chunk[4], chunk[3], chunk[2], chunk[1], chunk[0]);
         b = _mm256_add_epi32(a, b);
         let b:(i32,i32,i32,i32,i32,i32,i32,i32) = std::mem::transmute(b);
@@ -27,11 +31,13 @@ pub unsafe fn simd_add(arr:&mut [i32], val: i32) {
         chunk[7] = b.7;
     }
 
-    let rem = arr.chunks_exact_mut(8).into_remainder();
+    //normal_add(suffix, val);
 
-    for e in rem {
+    //let rem = arr.chunks_exact_mut(8).into_remainder();
+
+    /*for e in rem {
         *e += val;
-    }
+    }*/
 }
 
 #[cfg(test)]
