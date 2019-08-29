5322117f2ceac927389a8849988609f0fceacf03
Fasih Rana <fasih@apparicion.com>
2019-08-29 08:38:00 +0100

    including suggestions from leudz (https://users.rust-lang.org/t/simd-help-with-code-slower-in-release-mode/31440/6)

--- benches/example.rs
+++ benches/example.rs
@@ -14,11 +14,12 @@ fn normal_add_bench(b: &mut Bencher) {
 fn simd_add_bench(b:&mut Bencher) {
     unsafe {
         let mut arr = vec![1; len];
-        let (mut prefix, mut arr, mut suffix) = arr.align_to_mut::<i32>();
+        let (mut prefix, mut arr, mut suffix) = arr.align_to_mut::<[i32;8]>();
+        let arr: &mut [i32] = &mut *(arr as *mut _ as *mut _);
 
         b.iter(move || {
             normal_add(prefix, 1);
-            simd_add(&mut arr, 1);
+            simd_add(arr, 1);
             normal_add(suffix, 1);
         });
     }
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,22 @@ pub unsafe fn simd_add(arr:&mut [i32], val: i32) {
     #[cfg(target_arch = "x86_64")]
     use core::arch::x86_64::*;
 
-    //let (mut prefix, mut arr, mut suffix) = arr.align_to_mut::<i32>();
-    //normal_add(prefix, val);
-
-    let a = _mm256_set_epi32(val,val,val,val,val,val,val,val);
+    let a = _mm256_set1_epi32(val);
     for chunk in arr.chunks_exact_mut(8) {
 
-        let mut b =_mm256_set_epi32(chunk[7], chunk[6], chunk[5], chunk[4], chunk[3], chunk[2], chunk[1], chunk[0]);
+        let mut b =_mm256_set_epi32(
+            *chunk.get_unchecked(7),
+            *chunk.get_unchecked(6),
+            *chunk.get_unchecked(5),
+            *chunk.get_unchecked(4),
+            *chunk.get_unchecked(3),
+            *chunk.get_unchecked(2),
+            *chunk.get_unchecked(1),
+            *chunk.get_unchecked(0));
         b = _mm256_add_epi32(a, b);
-        let b:(i32,i32,i32,i32,i32,i32,i32,i32) = std::mem::transmute(b);
+        let b2 : *const i32 = &b as *const _ as *const _;
+        std::ptr::copy_nonoverlapping(b2, chunk.get_unchecked_mut(0), 8);
+        /*let b:(i32,i32,i32,i32,i32,i32,i32,i32) = std::mem::transmute(b);
         chunk[0] = b.0;
         chunk[1] = b.1;
         chunk[2] = b.2;
@@ -28,16 +35,8 @@ pub unsafe fn simd_add(arr:&mut [i32], val: i32) {
         chunk[4] = b.4;
         chunk[5] = b.5;
         chunk[6] = b.6;
-        chunk[7] = b.7;
+        chunk[7] = b.7;*/
     }
-
-    //normal_add(suffix, val);
-
-    //let rem = arr.chunks_exact_mut(8).into_remainder();
-
-    /*for e in rem {
-        *e += val;
-    }*/
 }
 
 #[cfg(test)]
